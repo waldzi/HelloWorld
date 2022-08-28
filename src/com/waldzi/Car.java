72611bf9a7f5b19ef8ccfde044ddaaac304a7926
waldzi <waldzi@poczta.fm>
2022-08-28 18:42:22 +0200

    Car class modified - setters and getters

--- src/com/waldzi/Car.java
+++ src/com/waldzi/Car.java
@@ -2,6 +2,24 @@ package com.waldzi;
 
 public class Car {
 
-    String manufacturer;
-    String model;
+    private String manufacturer;
+    private String model;
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+
 }
